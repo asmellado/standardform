bcfde127436b731ef09ff6c2c2eb19b87559e51a
alejandro.sanchez <asmellado@users.noreply.github.com>
2015-02-23 13:15:10 +0000

    Se crea el nuevo component StandardTable, que se separa de ListForm para poder ser reutilizado.

--- src/es/vegamultimedia/standardform/ListForm.java
+++ src/es/vegamultimedia/standardform/ListForm.java
@@ -2,16 +2,11 @@ package es.vegamultimedia.standardform;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
-import org.vaadin.dialogs.ConfirmDialog;
-
-import com.vaadin.data.Property;
 import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.event.ShortcutAction.KeyCode;
 import com.vaadin.shared.ui.label.ContentMode;
@@ -30,23 +25,24 @@ import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.Notification.Type;
 import com.vaadin.ui.Panel;
-import com.vaadin.ui.Table;
 import com.vaadin.ui.Table.ColumnGenerator;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.themes.BaseTheme;
 
 import es.vegamultimedia.standardform.DAO.BeanDAO;
 import es.vegamultimedia.standardform.DAO.SearchCriterion;
 import es.vegamultimedia.standardform.DAO.SearchCriterion.SearchType;
 import es.vegamultimedia.standardform.annotations.StandardForm;
-import es.vegamultimedia.standardform.annotations.StandardFormEnum;
 import es.vegamultimedia.standardform.annotations.StandardFormField;
+import es.vegamultimedia.standardform.components.StandardTable;
+import es.vegamultimedia.standardform.components.StandardTable.GeneratedColumn;
+import es.vegamultimedia.standardform.components.StandardTable.ShowDetailListener;
 import es.vegamultimedia.standardform.model.Bean;
 
-@SuppressWarnings("serial")
-public class ListForm<T extends Bean, K> extends Panel {
+public class ListForm<T extends Bean, K> extends Panel implements ShowDetailListener<T> {
 	
+	private static final long serialVersionUID = -8471432681552606031L;
+
 	/**
 	 * Interface for loading the table elements
 	 */
@@ -63,24 +59,6 @@ public class ListForm<T extends Bean, K> extends Panel {
     public interface BeforeCreateList<T extends Bean> {
         public abstract void beforeCreateList(List<T> elements);
     }
-
-    /**
-	 * Generated column of the list table
-	 */
-	public class GeneratedColumn {
-		private Object id;
-		private ColumnGenerator columnGenerator;
-		public GeneratedColumn(Object id, ColumnGenerator generatedColumn) {
-			this.id = id;
-			this.columnGenerator = generatedColumn;
-		}
-		public Object getId() {
-			return id;
-		}
-		public ColumnGenerator getColumnGenerator() {
-			return columnGenerator;
-		}
-	}
 	
 	protected BeforeCreateList<T> beforeCreateList; 
 	
@@ -127,15 +105,12 @@ public class ListForm<T extends Bean, K> extends Panel {
 	protected Label searchInfo;
 	
 	// Tabla para listado por defecto
-	protected Table table;
+	protected StandardTable<T, K> table;
 	
 	// Layout para listado personalizado
 	protected VerticalLayout listLayout;
 	
 	protected HorizontalLayout buttonsLayout;
-		
-	// Nombre columna editar
-	protected String nombreColumnaEditarConsultar;
 	
 	// Botón alta
 	protected Button addButton;
@@ -241,156 +216,17 @@ public class ListForm<T extends Bean, K> extends Panel {
 		}
 		// Si NO tiene customRowListComponent
 		if (standardFormAnnotation.customRowListComponent().isEmpty()) {
-			// Columnas de la tabla
-			List<String> visibledColumns;
-			// Creamos una tabla para mostrar los elementos
-			table = new Table(){
-				// Damos formato de sólo fecha a los campos de tipo Date 
-			    @Override
-			    protected String formatPropertyValue(Object rowId, Object colId, Property<?> property) {
-			        if (property.getType() == Date.class) {
-			            SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
-			            return df.format((Date)property.getValue());
-			        }
-			        else if (property.getType().isEnum()) {
-			        	// Obtenemos los elementos del enumerado
-						Object[] elementosEnum = property.getType().getEnumConstants();
-						// Recorremos todos los elementos del enumerado
-						for (Object elementoEnum: elementosEnum) {
-							// Se obtiene anotación StandardFormEnum del elemento
-							try {
-								java.lang.reflect.Field elementoField = property.getType().getField(elementoEnum.toString());
-								StandardFormEnum anotación = elementoField.getAnnotation(StandardFormEnum.class);
-								// Si tiene anotación StandardFormEnum informada
-								if (anotación != null && anotación.value().length() != 0) {
-									// Si el enumerado coincide con el valor la propiedad
-									if (elementoEnum == property.getValue())
-										// Se retorna el valor de la anotación
-										return anotación.value();
-								}
-							} catch (Exception e) {
-								e.printStackTrace();
-							}
-						}
-			        }
-			        else if (property.getType() == Boolean.class ||
-			        		property.getType() == Boolean.TYPE) {
-			        	if ((property.getValue() != null) && (Boolean)property.getValue() == true) {
-			        		return "Sí";
-			        	}
-			        	else {
-			        		return "No";
-			        	}
-			        }
-			        return super.formatPropertyValue(rowId, colId, property);
-			    }
-			};
-			table.setImmediate(true);
-			// TODO Parametrizar la longitud de la página
-			table.setPageLength(10);
-			
-			container = new BeanItemContainer<T>(beanUI.getBeanClass(), listElements);
-			table.setContainerDataSource(container);
-			
-			// Si se ha especificado columnas visibles personalizadas
-			if (customVisibledColumns != null) {
-				// Obtenemos todos los campos del bean
-				Field[] beanFields = Utils.getBeanFields(beanUI.getBeanClass());
-				// Se muestran las columnas especificadas (comprobando para cada una si existe el campo)
-				visibledColumns = new ArrayList<String>();
-				// Recorremos los campos
-				for (int i=0; i<customVisibledColumns.length; i++) {
-					// Obtenemos el campo que coincide con el nombre de la columna
-					for (Field beanField : beanFields) {
-						if (beanField.getName().equals(customVisibledColumns[i])) {
-							// Añadimos el campo a las columnas visibles
-							visibledColumns.add(customVisibledColumns[i]);
-							// Añadimos la cabecera de la columna
-							addHeaderColumn(beanField);
-							break;
-						}
-					}
-				}
-			}
-			// Si no se especifican las columnas visibles en la standardFormAnnotation
-			else if (standardFormAnnotation.columns()[0].isEmpty()) {
-				// Se muestran todas excepto el id
-				visibledColumns = new ArrayList<String>();
-				// Obtenemos sólo los campos declarados en el bean
-				Field[] beanFields = beanUI.getBeanClass().getDeclaredFields();
-				// Recorremos los campos
-				for (int i=0; i<beanFields.length; i++) {
-					// Si no es el id
-					if (!beanFields[i].getName().equals("id")) {
-						// Añadimos el campo a las columnas visibles
-						visibledColumns.add(beanFields[i].getName());
-						// Añadimos la cabecera de la columna
-						addHeaderColumn(beanFields[i]);
-					}
-				}
-			}
-			// Si se especifican las columnas visibles en la standardFormAnnotation
-			else {
-				// Hacemos visibles las columnas especificadas
-				visibledColumns = new ArrayList<String>(Arrays.asList(standardFormAnnotation.columns()));
-				// Obtenemos todos los campos del bean
-				Field[] beanFields = Utils.getBeanFields(beanUI.getBeanClass());
-				// Para cada columna, añadimos su cabecera
-				for(String nombreColumn : visibledColumns) {
-					// Obtenemos el campo que coincide con el nombre de la columna
-					for (Field beanField : beanFields) {
-						if (beanField.getName().equals(nombreColumn)) {
-							// Lo añadimos a la cabecera
-							addHeaderColumn(beanField);
-							break;
-						}
-					}
-				}
-			}
-			// Si se permite edición
-			if (standardFormAnnotation.allowsEditing()) {
-				nombreColumnaEditarConsultar = "Editar";
-			}
-			else {
-				nombreColumnaEditarConsultar = "Consultar";
-			}
-			// Si se permite consultar
-			if (allowConsulting) {
-				// Añadimos columna para editar o consultar
-				table.addGeneratedColumn(nombreColumnaEditarConsultar,
-						new EditColumnGenerator(nombreColumnaEditarConsultar));
-				visibledColumns.add(nombreColumnaEditarConsultar);
-			}
-			// Si se permite eliminar
-			if (standardFormAnnotation.allowsDeleting()) {
-				// Añadimos columna para eliminar
-				table.addGeneratedColumn("Eliminar", new DeleteColumnGenerator());
-				visibledColumns.add("Eliminar");
-			}
-			table.setVisibleColumns(visibledColumns.toArray());
-			
-			// Añadimos las columnas generadas personalizadas
-			for (GeneratedColumn column : generatedColumns) {
-				Object id = column.getId();
-				ColumnGenerator columnGenerator = column.getColumnGenerator();
-				table.addGeneratedColumn(id, columnGenerator);
-			}
-			// Si el formulario está deshabilitado
+
 			if (!formEnabled) {
-				// Quitamos columnas editar, consultar y eliminar
-				table.removeGeneratedColumn("Editar");
-				table.removeGeneratedColumn("Consultar");
-				table.removeGeneratedColumn("Eliminar");
-				// Si se permite consultar
-				if (allowConsulting) {
-					// Añadimos la columna consultar
-					table.addGeneratedColumn("Consultar", new EditColumnGenerator("Consultar"));
-				}
 				// Ocultamos el botón añadir
 				if (addButton != null) {
 					addButton.setVisible(false);
 				}
 			}
+			container = new BeanItemContainer<T>(beanUI.getBeanClass(), listElements);
+			// Creamos una nueva tabla (pues Vaadin da problemas si reusamos la misma)
+			table = new StandardTable<T, K>(container, beanUI, formEnabled, allowConsulting, 
+					customVisibledColumns, customColumnHeaders, generatedColumns, this);
 			listLayout.addComponent(table);
 		}
 		// Si tiene customRowListComponent
@@ -469,6 +305,8 @@ public class ListForm<T extends Bean, K> extends Panel {
 		Button searchButton = new Button("Buscar");
 		searchButton.setClickShortcut(KeyCode.ENTER);
 		searchButton.addClickListener(new ClickListener() {
+			private static final long serialVersionUID = 6657565320103922397L;
+
 			@Override
 			public void buttonClick(ClickEvent event) {
 				search();
@@ -629,30 +467,6 @@ public class ListForm<T extends Bean, K> extends Panel {
 	}
 
 	/**
-	 * Adds a header column for a bean field
-	 * @param beanField
-	 */
-	protected void addHeaderColumn(Field beanField) {
-		// Obtenemos la cabecera pesonalizada
-		String cabeceraPersonalizada = customColumnHeaders.get(beanField.getName());
-		// Si existe cabecera personalizada
-		if (cabeceraPersonalizada != null) {
-			// La ponemos como cabecera
-			table.setColumnHeader(beanField.getName(), cabeceraPersonalizada);
-			return;
-		}
-		// Obtenemos la anotación StandarFormField del campo
-		StandardFormField standardFormField = beanField.getAnnotation(StandardFormField.class);
-		// Si la columna tiene caption
-		if (standardFormField != null && standardFormField.caption().length() != 0)
-			// Ponemos el caption como cabecera
-			table.setColumnHeader(beanField.getName(), standardFormField.caption());
-		else
-			// Si no, ponemos el nombre del campo con la primera letra en mayúscula
-			table.setColumnHeader(beanField.getName(), Utils.capitalizeFirstLetter(beanField.getName()));
-	}
-	
-	/**
 	 * Gets every elements of this bean type using thee BeanDAO
 	 */
 	protected List<T> loadData() {
@@ -676,8 +490,9 @@ public class ListForm<T extends Bean, K> extends Panel {
 	 * Before, it gets again the bean using the BeanDAO (just in case it has changed)
 	 * @param element
 	 */
+	@Override
 	@SuppressWarnings("unchecked")
-	protected void showDetailForm(T element) {
+	public void showDetailForm(T element) {
 		Component vistaDetalle;
 		try {
 			// Si hay elemento
@@ -695,82 +510,6 @@ public class ListForm<T extends Bean, K> extends Panel {
 		}
 	}
 	
-	/*
-	 * Class for the edit column 
-	 */
-	public class EditColumnGenerator implements Table.ColumnGenerator {
-		
-		private String nameColumn;
-		
-		public EditColumnGenerator(String nameColumn) {
-			this.nameColumn = nameColumn;
-		}
-
-		private static final long serialVersionUID = 1L;
-
-		@Override
-		public Object generateCell(Table source, final Object itemId, Object columnId) {
-			Button button = new Button(nameColumn);
-			button.setStyleName(BaseTheme.BUTTON_LINK);
-			button.addClickListener(new ClickListener() {
-
-				private static final long serialVersionUID = 1L;
-
-				@Override
-				public void buttonClick(ClickEvent event) {
-					T elementoSeleccionado = container.getItem(itemId).getBean();
-					showDetailForm(elementoSeleccionado);
-				}
-			});
-			return button;
-		}
-	}
-	
-	/*
-	 * Class for the delete column
-	 */
-	public class DeleteColumnGenerator implements Table.ColumnGenerator {
-
-		private static final long serialVersionUID = 1L;
-
-		@Override
-		public Object generateCell(Table source, final Object itemId, Object columnId) {
-			Button button = new Button("Eliminar");
-			button.setStyleName(BaseTheme.BUTTON_LINK);
-			button.addClickListener(new ClickListener() {
-				
-				private static final long serialVersionUID = 1L;
-				
-				@Override
-				public void buttonClick(ClickEvent event) {
-					ConfirmDialog.show(getUI(), "Confirmación", 
-							"¿Está seguro de que desea eliminar el elemento?",
-					        "Sí", "No", new ConfirmDialog.Listener() {
-						private static final long serialVersionUID = 1L;
-
-						public void onClose(ConfirmDialog dialog) {
-			                if (dialog.isConfirmed()) {
-			                	T elementoSeleccionado = container.getItem(itemId).getBean();
-			                	try {
-			                		beanUI.getBeanDAO().remove(elementoSeleccionado);
-				                	Notification.show("Información",
-				                			"El elemento se ha eliminado correctamente",
-				                			Type.TRAY_NOTIFICATION);
-				                	// Eliminamos el item de la tabla
-				                	container.removeItem(itemId);
-			                	} catch (Exception e) {
-			    					Notification.show("No se ha podido eliminar el elemento",
-			    							e.getMessage(), Type.ERROR_MESSAGE);
-			    				}			                	
-			                }
-			            }
-			        });
-				}
-			});
-			return button;
-		}
-	}
-
 	/**
 	 * Disables this Listform:
 	 * Removes Edit and Delete columns, adds Consult column and hides the Add button.
@@ -789,31 +528,6 @@ public class ListForm<T extends Bean, K> extends Panel {
 	public void forbidConsulting() {
 		allowConsulting = false;
 	}
-	
-//	/**
-//	 * Hides the column with de id specified
-//	 * @param columnId It must be the fieldname of the bean
-//	 */
-//	public void hideVisibledColumn(String columnId) {
-//		ArrayList<Object> listVisibleColumns = new ArrayList<Object>();
-//		Field[] beanFields = Utils.getBeanFields(beanUI.getBeanClass());
-//		for (Object column : table.getVisibleColumns()) {
-//			// Si la columna visible no coincide con la que se quiere ocultar
-//			if (!columnId.equals(column)) {
-//				// Se añade a la nueva lista de columnas visibles
-//				listVisibleColumns.add(column);
-//				// Obtenemos el campo que coincide con el nombre de la columna
-//				for (Field beanField : beanFields) {
-//					if (beanField.getName().equals(column)) {
-//						// Lo añadimos a la cabecera
-//						addHeaderColumn(beanField);
-//						break;
-//					}
-//				}
-//			}
-//		}
-//		table.setVisibleColumns(listVisibleColumns.toArray());
-//	}
 	
 	/**
 	 * Shows all the elements in the list
---
+++ src/es/vegamultimedia/standardform/components/StandardTable.java
@@ -0,0 +1,345 @@
+package es.vegamultimedia.standardform.components;
+
+import java.lang.reflect.Field;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+import org.vaadin.dialogs.ConfirmDialog;
+
+import com.vaadin.data.Property;
+import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.Notification.Type;
+import com.vaadin.ui.Table;
+import com.vaadin.ui.themes.BaseTheme;
+
+import es.vegamultimedia.standardform.BeanUI;
+import es.vegamultimedia.standardform.Utils;
+import es.vegamultimedia.standardform.annotations.StandardForm;
+import es.vegamultimedia.standardform.annotations.StandardFormEnum;
+import es.vegamultimedia.standardform.annotations.StandardFormField;
+import es.vegamultimedia.standardform.model.Bean;
+
+public class StandardTable<T extends Bean, K> extends Table {
+	
+	private static final long serialVersionUID = 3412364081573747242L;
+
+	/**
+     * Interface for showing the detail form for a bean 
+     */
+    public interface ShowDetailListener<T extends Bean> {
+        /**
+         * Shows the DetailForm
+         * @param bean
+         */
+    	public abstract void showDetailForm(T bean);
+    }
+	
+    /**
+	 * Generated column of this StandardTable
+	 */
+	public static class GeneratedColumn {
+		private Object id;
+		private ColumnGenerator columnGenerator;
+		public GeneratedColumn(Object id, ColumnGenerator generatedColumn) {
+			this.id = id;
+			this.columnGenerator = generatedColumn;
+		}
+		public Object getId() {
+			return id;
+		}
+		public ColumnGenerator getColumnGenerator() {
+			return columnGenerator;
+		}
+	}
+	
+	// Escuchador para mostrar el detailForm
+	protected ShowDetailListener<T> showDetailListener;
+	
+	// BeanUI that created this standard list form
+	protected BeanUI<T, K> beanUI;
+	
+	// Container del formulario
+	protected BeanItemContainer<T> container;
+	
+	// Cabeceras de las columnas personalizadas
+	protected HashMap<String, String> customColumnHeaders;
+
+	public StandardTable(BeanItemContainer<T> container,
+			BeanUI<T, K> beanUI,
+			boolean formEnabled,
+			boolean allowConsulting, 
+			String[] customVisibledColumns,
+			HashMap<String, String> customColumnHeaders,
+			ArrayList<GeneratedColumn> customGeneratedColumns,
+			ShowDetailListener<T> showDetailListener) {
+		
+		// Inicializamos atributos
+		this.container = container;
+		this.beanUI = beanUI;
+		this.showDetailListener = showDetailListener;
+		this.customColumnHeaders = customColumnHeaders;
+
+		// Columnas de la tabla
+		List<String> visibledColumns;
+		
+		// Obtenemos la anotación StandardForm del bean
+		StandardForm standardFormAnnotation = beanUI.getBeanClass().getAnnotation(StandardForm.class);
+
+		
+		setImmediate(true);
+		// TODO Parametrizar la longitud de la página
+		setPageLength(10);
+		
+		setContainerDataSource(container);
+		
+		// Si se ha especificado columnas visibles personalizadas
+		if (customVisibledColumns != null) {
+			// Obtenemos todos los campos del bean
+			Field[] beanFields = Utils.getBeanFields(beanUI.getBeanClass());
+			// Se muestran las columnas especificadas (comprobando para cada una si existe el campo)
+			visibledColumns = new ArrayList<String>();
+			// Recorremos los campos
+			for (int i=0; i<customVisibledColumns.length; i++) {
+				// Obtenemos el campo que coincide con el nombre de la columna
+				for (Field beanField : beanFields) {
+					if (beanField.getName().equals(customVisibledColumns[i])) {
+						// Añadimos el campo a las columnas visibles
+						visibledColumns.add(customVisibledColumns[i]);
+						// Añadimos la cabecera de la columna
+						addHeaderColumn(beanField);
+						break;
+					}
+				}
+			}
+		}
+		// Si no se especifican las columnas visibles en la standardFormAnnotation
+		else if (standardFormAnnotation.columns()[0].isEmpty()) {
+			// Se muestran todas excepto el id
+			visibledColumns = new ArrayList<String>();
+			// Obtenemos sólo los campos declarados en el bean
+			Field[] beanFields = beanUI.getBeanClass().getDeclaredFields();
+			// Recorremos los campos
+			for (int i=0; i<beanFields.length; i++) {
+				// Si no es el id
+				if (!beanFields[i].getName().equals("id")) {
+					// Añadimos el campo a las columnas visibles
+					visibledColumns.add(beanFields[i].getName());
+					// Añadimos la cabecera de la columna
+					addHeaderColumn(beanFields[i]);
+				}
+			}
+		}
+		// Si se especifican las columnas visibles en la standardFormAnnotation
+		else {
+			// Hacemos visibles las columnas especificadas
+			visibledColumns = new ArrayList<String>(Arrays.asList(standardFormAnnotation.columns()));
+			// Obtenemos todos los campos del bean
+			Field[] beanFields = Utils.getBeanFields(beanUI.getBeanClass());
+			// Para cada columna, añadimos su cabecera
+			for(String nombreColumn : visibledColumns) {
+				// Obtenemos el campo que coincide con el nombre de la columna
+				for (Field beanField : beanFields) {
+					if (beanField.getName().equals(nombreColumn)) {
+						// Lo añadimos a la cabecera
+						addHeaderColumn(beanField);
+						break;
+					}
+				}
+			}
+		}
+		
+		String nombreColumnaEditarConsultar;
+		// Si se permite edición
+		if (standardFormAnnotation.allowsEditing()) {
+			nombreColumnaEditarConsultar = "Editar";
+		}
+		else {
+			nombreColumnaEditarConsultar = "Consultar";
+		}
+		// Si se permite consultar
+		if (allowConsulting) {
+			// Añadimos columna para editar o consultar 
+			// (Llamamos al método de la superclase para no registrarlo en generatedColumns)
+			super.addGeneratedColumn(nombreColumnaEditarConsultar,
+					new EditColumnGenerator(nombreColumnaEditarConsultar));
+			visibledColumns.add(nombreColumnaEditarConsultar);
+		}
+		// Si se permite eliminar
+		if (standardFormAnnotation.allowsDeleting()) {
+			// Añadimos columna para eliminar
+			// (Llamamos al método de la superclase para no registrarlo en generatedColumns)
+			super.addGeneratedColumn("Eliminar", new DeleteColumnGenerator());
+			visibledColumns.add("Eliminar");
+		}
+		setVisibleColumns(visibledColumns.toArray());
+		
+		// Añadimos las columnas generadas personalizadas
+		for (GeneratedColumn column : customGeneratedColumns) {
+			Object id = column.getId();
+			ColumnGenerator columnGenerator = column.getColumnGenerator();
+			addGeneratedColumn(id, columnGenerator);
+		}
+		// Si el formulario está deshabilitado
+		if (!formEnabled) {
+			// Quitamos columnas editar, consultar y eliminar
+			removeGeneratedColumn("Editar");
+			removeGeneratedColumn("Consultar");
+			removeGeneratedColumn("Eliminar");
+			// Si se permite consultar
+			if (allowConsulting) {
+				// Añadimos la columna consultar
+				addGeneratedColumn("Consultar", new EditColumnGenerator("Consultar"));
+			}
+		}
+	}
+	
+	// Damos formato de sólo fecha a los campos de tipo Date 
+    @Override
+    protected String formatPropertyValue(Object rowId, Object colId, Property<?> property) {
+        if (property.getType() == Date.class) {
+            SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+            return df.format((Date)property.getValue());
+        }
+        else if (property.getType().isEnum()) {
+        	// Obtenemos los elementos del enumerado
+			Object[] elementosEnum = property.getType().getEnumConstants();
+			// Recorremos todos los elementos del enumerado
+			for (Object elementoEnum: elementosEnum) {
+				// Se obtiene anotación StandardFormEnum del elemento
+				try {
+					java.lang.reflect.Field elementoField = property.getType().getField(elementoEnum.toString());
+					StandardFormEnum anotación = elementoField.getAnnotation(StandardFormEnum.class);
+					// Si tiene anotación StandardFormEnum informada
+					if (anotación != null && anotación.value().length() != 0) {
+						// Si el enumerado coincide con el valor la propiedad
+						if (elementoEnum == property.getValue())
+							// Se retorna el valor de la anotación
+							return anotación.value();
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+        }
+        else if (property.getType() == Boolean.class ||
+        		property.getType() == Boolean.TYPE) {
+        	if ((property.getValue() != null) && (Boolean)property.getValue() == true) {
+        		return "Sí";
+        	}
+        	else {
+        		return "No";
+        	}
+        }
+        return super.formatPropertyValue(rowId, colId, property);
+    }
+
+    /**
+	 * Adds a header column for a bean field
+	 * @param beanField
+	 */
+	protected void addHeaderColumn(Field beanField) {
+		// Obtenemos la cabecera pesonalizada
+		String cabeceraPersonalizada = customColumnHeaders.get(beanField.getName());
+		// Si existe cabecera personalizada
+		if (cabeceraPersonalizada != null) {
+			// La ponemos como cabecera
+			setColumnHeader(beanField.getName(), cabeceraPersonalizada);
+			return;
+		}
+		// Obtenemos la anotación StandarFormField del campo
+		StandardFormField standardFormField = beanField.getAnnotation(StandardFormField.class);
+		// Si la columna tiene caption
+		if (standardFormField != null && standardFormField.caption().length() != 0)
+			// Ponemos el caption como cabecera
+			setColumnHeader(beanField.getName(), standardFormField.caption());
+		else
+			// Si no, ponemos el nombre del campo con la primera letra en mayúscula
+			setColumnHeader(beanField.getName(), Utils.capitalizeFirstLetter(beanField.getName()));
+	}
+	
+	/*
+	 * Class for the edit column 
+	 */
+	public class EditColumnGenerator implements Table.ColumnGenerator {
+		
+		private String nameColumn;
+		
+		public EditColumnGenerator(String nameColumn) {
+			this.nameColumn = nameColumn;
+		}
+
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public Object generateCell(Table source, final Object itemId, Object columnId) {
+			Button button = new Button(nameColumn);
+			button.setStyleName(BaseTheme.BUTTON_LINK);
+			button.addClickListener(new ClickListener() {
+
+				private static final long serialVersionUID = 1L;
+
+				@SuppressWarnings({ "unchecked", "rawtypes" })
+				@Override
+				public void buttonClick(ClickEvent event) {
+					T elementoSeleccionado =
+							(T) ((BeanItemContainer)getContainerDataSource()).getItem(itemId).getBean();
+					showDetailListener.showDetailForm(elementoSeleccionado);
+				}
+			});
+			return button;
+		}
+	}
+	
+	/*
+	 * Class for the delete column
+	 */
+	public class DeleteColumnGenerator implements Table.ColumnGenerator {
+
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public Object generateCell(Table source, final Object itemId, Object columnId) {
+			Button button = new Button("Eliminar");
+			button.setStyleName(BaseTheme.BUTTON_LINK);
+			button.addClickListener(new ClickListener() {
+				
+				private static final long serialVersionUID = 1L;
+				
+				@Override
+				public void buttonClick(ClickEvent event) {
+					ConfirmDialog.show(getUI(), "Confirmación", 
+							"¿Está seguro de que desea eliminar el elemento?",
+					        "Sí", "No", new ConfirmDialog.Listener() {
+						private static final long serialVersionUID = 1L;
+
+						public void onClose(ConfirmDialog dialog) {
+			                if (dialog.isConfirmed()) {
+			                	T elementoSeleccionado = container.getItem(itemId).getBean();
+			                	try {
+			                		beanUI.getBeanDAO().remove(elementoSeleccionado);
+				                	Notification.show("Información",
+				                			"El elemento se ha eliminado correctamente",
+				                			Type.TRAY_NOTIFICATION);
+				                	// Eliminamos el item de la tabla
+				                	container.removeItem(itemId);
+			                	} catch (Exception e) {
+			    					Notification.show("No se ha podido eliminar el elemento",
+			    							e.getMessage(), Type.ERROR_MESSAGE);
+			    				}			                	
+			                }
+			            }
+			        });
+				}
+			});
+			return button;
+		}
+	}
+}
